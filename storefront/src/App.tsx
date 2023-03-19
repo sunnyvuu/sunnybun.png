ef55641164cbd615143f1fbe2ab253fd93456f2a
Madeline Yi <sunhunfunbun@gmail.com>
2023-03-18 19:07:14 -0500

    added navbar component

--- storefront/src/App.tsx
+++ storefront/src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import "./styles/index.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
+        <NavBar />
+      </BrowserRouter>
+
       <h1 className="text-center bg-slate-300 p-8">Hello World :3c</h1>
     </div>
   );
