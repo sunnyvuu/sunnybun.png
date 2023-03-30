b1129f7dbcf0b4f6860e1702b3fea5d621ed99d5
Madeline Yi <sunhunfunbun@gmail.com>
2023-03-30 17:37:33 -0500

    styled page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,15 @@ export const Home = () => {
   return (
     <div>
       HeroBanner
-      <div>
-        <h2>Best Selling Products</h2>
-        <p>Super Cute Jewelry</p>
+      <div className="text-center py-5">
+        <h2 className="text-3xl font-bold text-red-800 py-2">
+          Best Selling Products
+        </h2>
+        <p className="text-stone-500 text-lg">Super Cute Jewelry</p>
       </div>
-      <div>{["Product1", "Product2"].map((product) => product)}</div>
+      <div className="flex justify-evenly">
+        {["Product1", "Product2"].map((product) => product)}
+      </div>
       Footer
     </div>
   );
