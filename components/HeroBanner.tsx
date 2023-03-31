22ae92ed9949697412771700496aca68c62a87ca
Madeline Yi <sunhunfunbun@gmail.com>
2023-03-30 22:23:02 -0500

    added base info

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -1,5 +1,32 @@
 import React from "react";
+import Link from "next/link";
 
-export default function HeroBanner() {
-  return <div>HeroBanner</div>;
-}
+type CatFact = {
+  fact: string;
+  length: number;
+};
+
+const HeroBanner = (props: { catFactData: CatFact }) => {
+  const { catFactData } = props;
+
+  return (
+    <div>
+      <div>
+        <p> {catFactData.fact}</p>
+        <h3> MID TEXT </h3>
+        <img src="" alt="hairclips" />
+      </div>
+      <div>
+        <Link href="/product/ID">
+          <button type="button">BUTTON TEXT</button>
+        </Link>
+        <div>
+          <h5> Description </h5>
+          <p> DESCRIPTION </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroBanner;
