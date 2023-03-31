98f56ab31d3e2e3e5ca67a2fcb205325bb631772
Madeline Yi <sunhunfunbun@gmail.com>
2023-03-30 22:24:51 -0500

    added an interface for productprops

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
-export default function Product() {
-  return <div>Product</div>;
+interface ProductProps {
+  label: string;
+  price?: number;
+}
+
+export default function Product(props: ProductProps) {
+  return <div className="text-red-800">{props.label}</div>;
 }
