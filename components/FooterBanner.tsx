a103a65a30456420eec1cace1643c907c366249d
Madeline Yi <sunhunfunbun@gmail.com>
2023-04-02 17:48:12 -0500

    added footer stuff

--- components/FooterBanner.tsx
+++ components/FooterBanner.tsx
@@ -1,5 +1,42 @@
 import React from "react";
+import Link from "next/link";
+import { urlFor } from "@/lib/client";
 
-export default function FooterBanner() {
-  return <div>FooterBanner</div>;
+export default function FooterBanner(props: { footerBanner: any }) {
+  const { footerBanner } = props;
+  const {
+    discount,
+    largeText1,
+    largeText2,
+    saleTime,
+    smallText,
+    midText,
+    desc,
+    product,
+    buttonText,
+    image,
+  } = footerBanner;
+
+  return (
+    <div>
+      <div>
+        <div>
+          <p>{discount}</p>
+          <h3>{largeText1}</h3>
+          <h3>{largeText2}</h3>
+          <p>{saleTime}</p>
+        </div>
+
+        <div>
+          <p>{smallText}</p>
+          <h3>{midText}</h3>
+          <p>{desc}</p>
+          <Link href={`/product/${product}`}>
+            <button type="button">{buttonText}</button>
+          </Link>
+        </div>
+        <img src={urlFor(image) as unknown as string} />
+      </div>
+    </div>
+  );
 }
