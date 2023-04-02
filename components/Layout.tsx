6af9b25ecbe0f5ebd2d4781eef453f8b96243636
Madeline Yi <sunhunfunbun@gmail.com>
2023-04-02 17:46:23 -0500

    added layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
-export default function Layout() {
-  return <div>Layout</div>;
+export default function Layout(props: { children: any }) {
+  const { children } = props;
+
+  return (
+    <div>
+      <Head>
+        <title> SunnyBun.png </title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main>{children}</main>
+
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 }
