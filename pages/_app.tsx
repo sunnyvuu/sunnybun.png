6f35dec529c9c09bd797b5933abca5b72e392903
Madeline Yi <sunhunfunbun@gmail.com>
2023-04-02 17:46:10 -0500

    added layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import "@/styles/globals.css";
 import React from "react";
 import type { AppProps } from "next/app";
+import { Layout } from "@/components";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
