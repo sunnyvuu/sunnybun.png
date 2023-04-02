15d837e966152689b07faeba9f1b4bee3a24d6bf
Madeline Yi <sunhunfunbun@gmail.com>
2023-04-02 16:00:52 -0500

    fixed how props were passed and added styling

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -7,7 +7,8 @@ type CatFact = {
   length: number;
 };
 
-type bannerData = {
+type bannerDataItem = {
+  image: any;
   buttonText: string;
   desc: string;
   discount: string;
@@ -24,22 +25,44 @@ type bannerData = {
   _updatedAt: string;
 };
 
-const HeroBanner = (heroBanner: any) => {
+const HeroBanner = (props: { heroBanner: bannerDataItem }) => {
+  const { heroBanner } = props;
+  console.log(urlFor(heroBanner.image));
+
   return (
-    <div>
+    <div className="flex justify-between pt-1 pr-4 relative bg-slate-400 m-10 rounded-xl">
       <div>
-        <p className="bg-slate-500">{heroBanner.smallText}</p>
-        <h3>{heroBanner.midText}</h3>
-        <h1>{heroBanner.largeText1}</h1>
-        <img src="" alt="hairclips" />
+        <p className="m-18px m-10 text-2xl">{heroBanner.smallText}</p>
+        <h3 className="mt-4px text-5xl m-10 font-medium">
+          {heroBanner.midText}
+        </h3>
+        <h1 className="text-3xl uppercase m-10 text-white">
+          {heroBanner.largeText1}
+        </h1>
       </div>
-      <div>
+      <img
+        src={urlFor(heroBanner.image) as unknown as string}
+        alt="hairclips"
+        className="m-4 max-h-[36rem]"
+      />
+
+      <div className="">
         <Link href={`/product/ID${heroBanner.product}`}>
-          <button type="button">{heroBanner.buttonText}</button>
+          <button
+            className="m-10 rounded-xl px-5 py-2 bg-sky-100 text-sky-700 border-none mt-40px text-base font-medium"
+            type="button"
+          >
+            {heroBanner.buttonText}
+          </button>
         </Link>
-        <div>
-          <h5> Description </h5>
-          <p>{heroBanner.desc}</p>
+        <div className="mt-10 absolute right-3 bottom-0 leading-5 flex flex-col c-red-500">
+          <h5 className="mt-10 mr-10 font-bold text-2xl self-end">
+            {" "}
+            Description{" "}
+          </h5>
+          <p className="mt-2 ml-5 mr-10 mb-10 font-extralight text-end w-96 text-base">
+            {heroBanner.desc}
+          </p>
         </div>
       </div>
     </div>
