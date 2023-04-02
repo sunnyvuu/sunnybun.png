fe4273cf6d5f7909e4c10527b85ffb2f092bf848
Madeline Yi <sunhunfunbun@gmail.com>
2023-04-02 17:57:04 -0500

    Added footer details

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <p> 2023 SunnBun.png All rights reserved</p>
+      <p>
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  );
 }
