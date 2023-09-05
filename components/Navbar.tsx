79e967ec4724a2a7dbdca359e8ef481dc0932c16
Madeline Yi <sunhunfunbun@gmail.com>
2023-09-04 22:07:40 -0500

    styled navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,13 +4,18 @@ import { AiOutlineShopping } from "react-icons/ai";
 
 export default function Navbar() {
   return (
-    <div>
-      <p>
+    <div className="flex justify-between my-5 mx-7 relative">
+      <p className="text-xl font-bold">
         <Link href="/"> SunnyBun.png Jewelry</Link>
       </p>
-      <button type="button" onClick="">
+      <button
+        type="button"
+        className="text-[25px] text-[gray] cursor-pointer relative transition-transform duration-[0.4s] ease-[ease] bg-transparent border-[none]"
+      >
         <AiOutlineShopping />
-        <span>1</span>
+        <span className="absolute text-xs text-[#eee] bg-[#f02d34] w-[18px] h-[18px] text-center font-semibold rounded-[50%] -right-2">
+          1
+        </span>
       </button>
     </div>
   );
