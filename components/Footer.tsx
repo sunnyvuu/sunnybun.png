af961dae408ebfbb2fd76594d2e2106ec9614a8c
Madeline Yi <sunhunfunbun@gmail.com>
2023-09-04 22:07:46 -0500

    styled footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,9 +3,9 @@ import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 
 export default function Footer() {
   return (
-    <div>
+    <div className="text-[#324d67] text-center font-bold flex flex-col items-center gap-2.5 justify-center mt-5 px-2.5 py-[30px]">
       <p> 2023 SunnBun.png All rights reserved</p>
-      <p>
+      <p className="text-3xl flex gap-2.5">
         <AiFillInstagram />
         <AiOutlineTwitter />
       </p>
